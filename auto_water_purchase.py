474fab23430ebb500b1b184f70aacf3f081b5f18
Tyler Osborn <tylerosborn1999@gmail.com>
2025-08-01 17:11:12 +0200

    add headless param

--- auto_water_purchase.py
+++ auto_water_purchase.py
@@ -46,7 +46,7 @@ class AutoWaterPurchase:
     def setup_driver(self):
         """Initialize Chrome WebDriver with appropriate options"""
         chrome_options = Options()
-        # chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--disable-gpu')
